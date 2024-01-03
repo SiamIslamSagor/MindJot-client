3e56768b9e1c4cda8dd07455a3305bc13a1c9446
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-03 18:48:14 +0600

    added a demo sign up page with style

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,38 @@
+import { Helmet } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
   return (
-    <div className="flex flex-col item-center justify-center border p-5 my-10">
-      <h1 className="text-4xl text-center font-bold">SignUp</h1>
+    <div>
+      <Helmet>
+        <title>MindJot | Sign Up</title>
+      </Helmet>
+      <Toaster></Toaster>
+
+      {/* main */}
+      <div className="px-5">
+        <div className=" mt-40 rounded-md max-w-md mx-auto w-full">
+          <h2 className="text-3xl my-10 font-bold">Sign up to MindJot</h2>
+          <button className="btn bg-light-black text-white text-base opacity-100 hover:bg-light-black hover:opacity-90 rounded-full w-full font-bold h-16 mt-6">
+            <FcGoogle className="text-2xl" />
+            Sign up with Google
+          </button>
+
+          <div className="divider my-8 text-slate-400">or</div>
+
+          <button className="btn bg-white opacity-90 hover:bg-white hover:opacity-100 rounded-full w-full font-bold h-16 text-base">
+            Continue with email
+          </button>
+          <p className="text-center my-5">
+            Already have an account?{" "}
+            <Link to={"/auth/signIn"} className="underline">
+              Sign In
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
