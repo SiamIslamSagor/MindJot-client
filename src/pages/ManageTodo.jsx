97c2d1653357d43ec658dd04473face936b50a1b
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-07 23:25:59 +0600

    create task page layout with proper design demo

--- src/pages/ManageTodo.jsx
+++ src/pages/ManageTodo.jsx
@@ -1,28 +1,231 @@
+import { Fade } from "react-awesome-reveal";
 import CreateButton from "../components/CreateButton";
+import { ScrollShadow } from "@nextui-org/react";
 
 const ManageTodo = () => {
   return (
     <div>
-      <div>
+      <Fade>
         <div className="flex justify-center flex-wrap gap-5 my-20">
-          <div className="bg-base-300 min-h-96 min-w-96 h-8  rounded-xl p-4">
-            <h3 className="bg-deep-rose rounded-lg text-white text-center p-3 text-2xl">
+          <ScrollShadow
+            orientation="horizontal"
+            hideScrollBar={true}
+            as={"div"}
+            className="bg-base-300 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4"
+          >
+            <div className="h-6 rounded-tmd sticky -top-4 bg-base-300"></div>
+            <h3 className="bg-deep-rose rounded-lg text-white text-center p-3 text-2xl sticky top-0 max-2xsm:min-w-72 max-xsm:min-w-80 xsm:min-w-96">
               Todo
             </h3>
-          </div>
-          <div className="bg-base-300 min-h-96 min-w-96 h-8  rounded-xl p-4">
+            <ul>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+              <li className="text-center my-2 bg-deep-rose   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Todo
+              </li>
+            </ul>
+          </ScrollShadow>
+          {/* <div className="bg-base-300 min-h-96 max-h-[65vh] min-w-96  rounded-xl p-4">
             <h3 className="bg-deep-green rounded-lg text-white text-center p-3 text-2xl">
               Ongoing
             </h3>
+            <ul>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+              <li className="text-center my-2 bg-deep-green   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Ongoing
+              </li>
+            </ul>
           </div>
-          <div className="bg-base-300 min-h-96 min-w-96 h-8  rounded-xl p-4">
+          <div className="bg-base-300 min-h-96 max-h-[65vh] min-w-96  rounded-xl p-4">
             <h3 className="bg-light-blue rounded-lg text-white text-center p-3 text-2xl">
               Completed
             </h3>
-          </div>
+            <ul>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+              <li className="text-center my-2 bg-light-blue   font-bold text-lg p-2 rounded-md bg-opacity-25">
+                Completed
+              </li>
+            </ul>
+          </div> */}
         </div>
-      </div>
-      <CreateButton tooltip="Create new todo" />
+        <CreateButton tooltip="Create new todo" />
+      </Fade>
     </div>
   );
 };
--- src/pages/StickyWall.jsx
+++ src/pages/StickyWall.jsx
@@ -2,44 +2,47 @@ import { useDisclosure } from "@nextui-org/react";
 import StickyForm from "../components/StickyForm";
 import StickyNote from "../components/StickyNote";
 import CreateButton from "../components/CreateButton";
+import { Fade } from "react-awesome-reveal";
 
 const StickyWall = () => {
   // nextUI modal hook
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   return (
-    <div className="px-2 md:px-5 ">
-      <h1 className="text-4xl text-center font-bold my-10">StickyWall</h1>
+    <Fade>
+      <div className="px-2 md:px-5 ">
+        <h1 className="text-4xl text-center font-bold my-10">StickyWall</h1>
 
-      {/* sticky notes */}
-      <div className="flex itemscenter justify-center gap-4 flex-wrap">
-        <StickyNote
-          color="blue"
-          description={
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli tia sunt velit adipisci eos fuga beatae vel eaque. Voluptatum id perferendis magni expedita at velit, aliquid nostrum dolorem natus harum amet, repellat beatae mollitia illo, quos rerum quam distinctio a quod modi recusandae tempora nobis animi. Maxime et tenetur eligendi ullam possimus. Fugiat, illo minus porro ab deleniti molestiae! Tempora dignissimos nemo odio libero sed porro magnam voluptatum consequatur ab possimus facere accusamus quod, magni voluptas dolore animi odit. Ipsum sint, perspiciatis quos impedit nostrum obcaecati consequatur. Eaque necessitatibus consequuntur sint architecto est fugit de"
-          }
-        />
-        <StickyNote
-          color="pink"
-          description={
-            " Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli tia sunt velit adipisci eos fuga beatae vel eaque. Voluptatum id perferendis magni expedita at velit, aliquid nostrum dolorem natus harum amet, repellat beatae mollitia illo, quos rerum quam distinctio a quod modi recusandae tempora nobis animi. Maxime et tenetur eligendi ullam possimus. Fugiat, illo minus porro ab deleniti molestiae! Tempora dignissimos nemo odio libero sed porro magnam voluptatum consequatur ab possimus facere accusamus quod, magni voluptas dolore animi odit. Ipsum sint, perspiciatis quos impedit nostrum obcaecati consequatur. Eaque necessitatibus consequuntur sint architecto est fugit debitis non suscipit?"
-          }
-        />
-        <StickyNote color="black" />
-        <StickyNote color="light pink" />
-        <StickyNote color="blue" />
-        <StickyNote color="blue" />
-        <StickyNote color="light pink" />
-        <StickyNote color="green" />
-        <StickyNote color="black" />
+        {/* sticky notes */}
+        <div className="flex justify-center gap-4 flex-wrap">
+          <StickyNote
+            color="blue"
+            description={
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli tia sunt velit adipisci eos fuga beatae vel eaque. Voluptatum id perferendis magni expedita at velit, aliquid nostrum dolorem natus harum amet, repellat beatae mollitia illo, quos rerum quam distinctio a quod modi recusandae tempora nobis animi. Maxime et tenetur eligendi ullam possimus. Fugiat, illo minus porro ab deleniti molestiae! Tempora dignissimos nemo odio libero sed porro magnam voluptatum consequatur ab possimus facere accusamus quod, magni voluptas dolore animi odit. Ipsum sint, perspiciatis quos impedit nostrum obcaecati consequatur. Eaque necessitatibus consequuntur sint architecto est fugit de"
+            }
+          />
+          <StickyNote
+            color="pink"
+            description={
+              " Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quaerat at laborum ratione esse perspiciatis neque, excepturi sunt, quia fuga natus itaque sit, nobis asperiores beatae magnam! Soluta autem ea praesentium deleniti sit facilis cupiditate. Fugit, unde eos? Nequas itaque ut voluptates amet, nisi, minus molli tia sunt velit adipisci eos fuga beatae vel eaque. Voluptatum id perferendis magni expedita at velit, aliquid nostrum dolorem natus harum amet, repellat beatae mollitia illo, quos rerum quam distinctio a quod modi recusandae tempora nobis animi. Maxime et tenetur eligendi ullam possimus. Fugiat, illo minus porro ab deleniti molestiae! Tempora dignissimos nemo odio libero sed porro magnam voluptatum consequatur ab possimus facere accusamus quod, magni voluptas dolore animi odit. Ipsum sint, perspiciatis quos impedit nostrum obcaecati consequatur. Eaque necessitatibus consequuntur sint architecto est fugit debitis non suscipit?"
+            }
+          />
+          <StickyNote color="black" />
+          <StickyNote color="light pink" />
+          <StickyNote color="blue" />
+          <StickyNote color="blue" />
+          <StickyNote color="light pink" />
+          <StickyNote color="green" />
+          <StickyNote color="black" />
+        </div>
+
+        {/* create new sticky note button with proper style and animation */}
+        <CreateButton onOpen={onOpen} tooltip={"Create new sticky note"} />
+
+        {/* sticky form */}
+        <StickyForm isOpen={isOpen} onOpenChange={onOpenChange}></StickyForm>
       </div>
-
-      {/* create new sticky note button with proper style and animation */}
-      <CreateButton onOpen={onOpen} tooltip={"Create new sticky note"} />
-
-      {/* sticky form */}
-      <StickyForm isOpen={isOpen} onOpenChange={onOpenChange}></StickyForm>
-    </div>
+    </Fade>
   );
 };
 
