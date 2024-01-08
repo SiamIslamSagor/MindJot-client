bffbad47a37f421513e2498e1b1b0e68674e954a
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-09 01:18:42 +0600

    fix style bugs in task page with dnd

--- src/pages/ManageTodo.jsx
+++ src/pages/ManageTodo.jsx
@@ -42,79 +42,23 @@ const ManageTodo = () => {
       <div>
         <Fade>
           <div className="flex justify-center flex-wrap gap-10 my-20">
-            {/* todo */}
-            {/* <ScrollShadow
-              orientation="horizontal"
-              hideScrollBar={true}
-              as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
-            >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Todo"} taskType={"todo"} />
-              <Droppable droppableId="todo-area-old" key={"todo-area"}>
-                {provided => (
-                  <Accordion
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                    variant="splitted"
-                  >
-                    {taskData
-                      .filter(task => task.status === "todo")
-                      .map((task, index) => (
-                        <AccordionItem
-                          key={task._id}
-                          aria-label={task.heading}
-                          title={task.heading}
-                          className="max-w-[350px] mx-2 relative "
-                        >
-                          <Draggable
-                            key={task._id}
-                            draggableId={task._id}
-                            index={index}
-                          >
-                            {(provided, snapshot) => (
-                              <p
-                                ref={provided.innerRef}
-                                {...provided.draggableProps}
-                                {...provided.dragHandleProps}
-                                className={`pb-12 ${
-                                  snapshot.isDragging ? "bg-red-200" : ""
-                                }`}
-                                style={{
-                                  userSelect: "none",
-                                  ...provided.draggableProps.style,
-                                }}
-                              >
-                                {task.description}
-                              </p>
-                            )}
-                          </Draggable>
-                          <ThreeDot />
-                          {provided.placeholder}
-                        </AccordionItem>
-                      ))}
-                  </Accordion>
-                )}
-              </Droppable>
-            </ScrollShadow> */}
             {/* todo with customized accordion */}
             <ScrollShadow
               orientation="horizontal"
               hideScrollBar={true}
               as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
+              className="bg-base-200 bg-opacity-50 shadow-xl border min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl duration-500"
             >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Todo"} taskType={"todo"} />
               <Droppable droppableId="todo-area" key={"todo-area"}>
                 {(provided, snapshot) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className={`space-y-2 duration-400 rounded-lg ${
+                    className={`space-y-2 pt-8 p-4 pb-14 duration-400 rounded-lg ${
                       snapshot.isDraggingOver && "bg-base-300"
                     }`}
                   >
+                    <TaskHeading text={"Todo"} taskType={"todo"} />
                     {taskData
                       .filter(task => task.status === "todo")
                       .map((task, index) => (
@@ -165,19 +109,18 @@ const ManageTodo = () => {
               orientation="horizontal"
               hideScrollBar={true}
               as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
+              className="bg-base-200 bg-opacity-50 shadow-xl border min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl duration-500"
             >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Ongoing"} taskType={"ongoing"} />
               <Droppable droppableId="ongoing-area" key={"ongoing-area"}>
                 {(provided, snapshot) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className={`space-y-2 duration-400 rounded-lg ${
+                    className={`space-y-2 pt-8 p-4 pb-14 duration-400 rounded-lg ${
                       snapshot.isDraggingOver && "bg-base-300"
                     }`}
                   >
+                    <TaskHeading text={"Ongoing"} taskType={"ongoing"} />
                     {taskData
                       .filter(task => task.status === "ongoing")
                       .map((task, index) => (
@@ -228,19 +171,18 @@ const ManageTodo = () => {
               orientation="horizontal"
               hideScrollBar={true}
               as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
+              className="bg-base-200 bg-opacity-50 shadow-xl border min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl duration-500"
             >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Completed"} taskType={"completed"} />
               <Droppable droppableId="completed-area" key={"completed-area"}>
                 {(provided, snapshot) => (
                   <div
                     {...provided.droppableProps}
                     ref={provided.innerRef}
-                    className={`space-y-2 duration-400 rounded-lg ${
+                    className={`space-y-2 pt-8 p-4 pb-14 duration-400 rounded-lg ${
                       snapshot.isDraggingOver && "bg-base-300"
                     }`}
                   >
+                    <TaskHeading text={"Completed"} taskType={"completed"} />
                     {taskData
                       .filter(task => task.status === "completed")
                       .map((task, index) => (
@@ -286,90 +228,6 @@ const ManageTodo = () => {
                 )}
               </Droppable>
             </ScrollShadow>
-
-            {/*  */}
-            {/*  */}
-            {/*  */}
-            {/* ongoing with customized accordion */}
-            {/* <ScrollShadow
-              orientation="horizontal"
-              hideScrollBar={true}
-              as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
-            >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Ongoing"} taskType={"ongoing"} />
-              <div className=" space-y-2">
-                {taskData
-                  .filter(task => task.status === "ongoing")
-                  .map(task => (
-                    <div
-                      className="max-w-[350px] mx-auto bg-white shadow-2xl p-4 rounded-2xl duration-400"
-                      key={task._id}
-                    >
-                      <h4
-                        onClick={handlerAccordion}
-                        className="flex items-center justify-between text-lg   cursor-pointer py-1 select-none"
-                      >
-                        {task.heading}{" "}
-                        <FaAngleLeft className="duration-300 pointer-events-none opacity-50 text-base" />
-                      </h4>
-                      <div className="description Div hidden overflow-hidden">
-                        <Fade direction="down">
-                          <p className="py-3">{task.description}</p>
-                          <hr className="mb-2" />
-                          <p className="flex items-center justify-between">
-                            status: {task.status}{" "}
-                            <span>
-                              <HiDotsVertical />
-                            </span>
-                          </p>
-                        </Fade>
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            </ScrollShadow> */}
-            {/* completed with customized accordion */}
-            {/* <ScrollShadow
-              orientation="horizontal"
-              hideScrollBar={true}
-              as={"div"}
-              className="bg-base-200 min-h-96 max-h-[65vh] max-xsm: xsm:min-w-96  rounded-xl p-4 duration-500"
-            >
-              <div className="z-10 h-6 rounded-tmd sticky -top-4 bg-base-200"></div>
-              <TaskHeading text={"Completed"} taskType={"completed"} />
-              <div className=" space-y-2">
-                {taskData
-                  .filter(task => task.status === "completed")
-                  .map(task => (
-                    <div
-                      className="max-w-[350px] mx-auto bg-white shadow-2xl p-4 rounded-2xl duration-400"
-                      key={task._id}
-                    >
-                      <h4
-                        onClick={handlerAccordion}
-                        className="flex items-center justify-between text-lg   cursor-pointer py-1 select-none"
-                      >
-                        {task.heading}{" "}
-                        <FaAngleLeft className="duration-300 pointer-events-none opacity-50 text-base" />
-                      </h4>
-                      <div className="description Div hidden overflow-hidden">
-                        <Fade direction="down">
-                          <p className="py-3">{task.description}</p>
-                          <hr className="mb-2" />
-                          <p className="flex items-center justify-between">
-                            status: {task.status}{" "}
-                            <span>
-                              <HiDotsVertical />
-                            </span>
-                          </p>
-                        </Fade>
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            </ScrollShadow> */}
           </div>
         </Fade>
         <CreateButton tooltip="Create new todo" />
