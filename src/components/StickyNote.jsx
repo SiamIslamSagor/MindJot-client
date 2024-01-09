e8e9ed0d2fc69b7e60abf965ceb0117fdd598a9e
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-09 19:04:18 +0600

    back to previous state and remeove grid dnd functionality cuz it's not responsive and when i try to responsive i fail, so i back to previous state

--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -10,29 +10,17 @@ import {
 import { AiOutlineDelete } from "react-icons/ai";
 import { FiEdit3 } from "react-icons/fi";
 import { HiDotsVertical } from "react-icons/hi";
+import { Draggable } from "react-beautiful-dnd";
 
 const StickyNote = ({ color = "green", heading, description, _id, index }) => {
   return (
-    <ScrollShadow
-      className={`relative max-sm:min-h-72 sm:min-h-80 max-h-96  max-sm:min-w-72 sm:min-w-80 max-w-96 p-5 pb-16 shadow-2xl text-white rounded-lg ${
-        color === "green"
-          ? "bg-deep-green"
-          : color === "blue"
-          ? "bg-light-blue"
-          : color === "black"
-          ? "bg-light-black"
-          : color === "pink"
-          ? "bg-deep-rose"
-          : color === "dark-black"
-          ? "bg-black"
-          : "bg-black"
-      }  `}
-      hideScrollBar={true}
-      as={"div"}
-    >
-      <div className="relative ">
-        <div
-          className={`sticky -top-5  flex items-center  mb-2 justify-between py-2 ${
+    <Draggable key={_id} draggableId={_id} index={index}>
+      {(provided, snapshot) => (
+        <ScrollShadow
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className={`relative max-sm:min-h-72 sm:min-h-80 max-h-96  max-sm:min-w-72 sm:min-w-80 max-w-96 p-5 pb-16 shadow-2xl text-white rounded-lg ${
             color === "green"
               ? "bg-deep-green"
               : color === "blue"
@@ -44,37 +32,58 @@ const StickyNote = ({ color = "green", heading, description, _id, index }) => {
               : color === "dark-black"
               ? "bg-black"
               : "bg-black"
-          } `}
+          } ${snapshot.isDragging ? "drop-shadow-2xl" : ""}`}
+          orientation="horizontal"
+          hideScrollBar={true}
+          as={"div"}
         >
-          <h3 className="text-center text-2xl md:font-bold max-w-72">
-            {heading ? heading : "Heading ..."}
-          </h3>{" "}
-          {/* action div */}
-          <div className="">
-            <Dropdown>
-              <DropdownTrigger>
-                <Button isIconOnly radius="full" variant="light">
-                  <HiDotsVertical className={`text-xl text-white`} />
-                </Button>
-              </DropdownTrigger>
-              <DropdownMenu aria-label="Static Actions">
-                <DropdownItem startContent={<FiEdit3 />}>
-                  Edit Note
-                </DropdownItem>
-                <DropdownItem
-                  className="text-danger"
-                  color="danger"
-                  startContent={<AiOutlineDelete />}
-                >
-                  Delete Note
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
+          <div className="relative ">
+            <div
+              className={`sticky -top-5  flex items-center  mb-2 justify-between py-2 ${
+                color === "green"
+                  ? "bg-deep-green"
+                  : color === "blue"
+                  ? "bg-light-blue"
+                  : color === "black"
+                  ? "bg-light-black"
+                  : color === "pink"
+                  ? "bg-deep-rose"
+                  : color === "dark-black"
+                  ? "bg-black"
+                  : "bg-black"
+              } `}
+            >
+              <h3 className="text-center text-2xl md:font-bold max-w-72">
+                {heading ? heading : "Heading ..."}
+              </h3>{" "}
+              {/* action div */}
+              <div className="">
+                <Dropdown>
+                  <DropdownTrigger>
+                    <Button isIconOnly radius="full" variant="light">
+                      <HiDotsVertical className={`text-xl text-white`} />
+                    </Button>
+                  </DropdownTrigger>
+                  <DropdownMenu aria-label="Static Actions">
+                    <DropdownItem startContent={<FiEdit3 />}>
+                      Edit Note
+                    </DropdownItem>
+                    <DropdownItem
+                      className="text-danger"
+                      color="danger"
+                      startContent={<AiOutlineDelete />}
+                    >
+                      Delete Note
+                    </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </div>
+            </div>
+            <p className=" ">DESCRIPTION : {description}</p>
           </div>
-        </div>
-        <p className=" ">DESCRIPTION : {description}</p>
-      </div>
-    </ScrollShadow>
+        </ScrollShadow>
+      )}
+    </Draggable>
   );
 };
 
--- src/pages/StickyWall.jsx
+++ src/pages/StickyWall.jsx
@@ -1,68 +1,71 @@
-import { Button, useDisclosure } from "@nextui-org/react";
+import { useDisclosure } from "@nextui-org/react";
 import StickyForm from "../components/StickyForm";
 import StickyNote from "../components/StickyNote";
 import CreateButton from "../components/CreateButton";
 import { Fade } from "react-awesome-reveal";
 import { stickyNotesData } from "../links/dummyLinks";
-import {
-  GridContextProvider,
-  GridDropZone,
-  GridItem,
-  swap,
-} from "react-grid-dnd";
-import { useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+const onDragEnd = result => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    console.log("source.droppableId !== destination.droppableId");
+    console.log(result);
+  } else {
+    console.log("else block (in same status div)");
+    console.log(result);
+  }
+};
 
 const StickyWall = () => {
   // nextUI modal hook
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-  const [notesData, setNotesData] = useState(stickyNotesData);
-  function onChange(sourceId, sourceIndex, targetIndex) {
-    const nextState = swap(stickyNotesData, sourceIndex, targetIndex);
-    setNotesData(nextState);
-  }
-
   return (
     <Fade>
-      <div className="px-2 md:px-5 ">
-        <h1 className="text-4xl text-center font-bold my-10">StickyWall</h1>
+      <DragDropContext onDragEnd={result => onDragEnd(result)}>
+        <div className="px-2 md:px-5 ">
+          <h1 className="text-4xl text-center font-bold my-10">StickyWall</h1>
 
-        {/* sticky notes */}
-        <div className={`  `}>
-          <GridContextProvider onChange={onChange}>
-            <GridDropZone
-              id="nots"
-              boxesPerRow={3}
-              rowHeight={320}
-              style={{ height: 320 * Math.ceil(notesData.length / 3) }}
-            >
-              {notesData.map((note, index) => (
-                <GridItem key={note._id}>
+          {/* sticky notes */}
+          <Droppable
+            droppableId="sticky-notes-area"
+            key="sticky-notes-area"
+            type="droppableListItem"
+            direction="vertical"
+          >
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className={`flex justify-center gap-10 flex-wrap ${
+                  snapshot.isDraggingOver && "bg-base-300"
+                }`}
+              >
+                {stickyNotesData.map((note, index) => (
                   <StickyNote
-                    key={`note._id ${index}`}
+                    key={note._id}
                     index={index}
                     _id={note._id}
                     color={note.themeColor}
                     heading={note.heading}
                     description={note.description}
                   />
-                </GridItem>
-              ))}
-            </GridDropZone>
-          </GridContextProvider>
-          <div className="my-20 text-center">
-            <Button onClick={() => console.log(notesData)} color="primary">
-              State
-            </Button>
-          </div>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+
+          {/* create new sticky note button with proper style and animation */}
+          <CreateButton onOpen={onOpen} tooltip={"Create new sticky note"} />
+
+          {/* sticky form */}
+          <StickyForm isOpen={isOpen} onOpenChange={onOpenChange}></StickyForm>
         </div>
-
-        {/* create new sticky note button with proper style and animation */}
-        <CreateButton onOpen={onOpen} tooltip={"Create new sticky note"} />
-
-        {/* sticky form */}
-        <StickyForm isOpen={isOpen} onOpenChange={onOpenChange}></StickyForm>
-      </div>
+      </DragDropContext>
     </Fade>
   );
 };
