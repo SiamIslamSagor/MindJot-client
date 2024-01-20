79770ec09e7f78d2d95b5e110a52fe6a11fec1fe
SiamIslamSagor <msiamislam12345@gmail.com>
2024-01-20 22:22:06 +0600

    added hello world in thought page

--- src/pages/Thoughts.jsx
+++ src/pages/Thoughts.jsx
@@ -2,6 +2,7 @@ const Thoughts = () => {
   return (
     <div className="flex flex-col item-center justify-center border p-5 my-10">
       <h1 className="text-4xl text-center font-bold">Thoughts</h1>
+      <p>hello world</p>
     </div>
   );
 };
