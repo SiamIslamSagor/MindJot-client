4cc388e946e121f84b471dcee4de0f3b7705aa8a
SiamIslamSagor <msiamislam12345@gmail.com>
2024-02-06 23:48:40 +0600

    create blog 2

--- src/pages/CreateBlog.jsx
+++ src/pages/CreateBlog.jsx
@@ -2,6 +2,7 @@ const CreateBlog = () => {
   return (
     <div className="flex flex-col item-center justify-center border p-5 my-10">
       <h1 className="text-4xl text-center font-bold">CreateBlog</h1>
+      <h1 className="text-4xl text-center font-bold">CreateBlog</h1>
     </div>
   );
 };
