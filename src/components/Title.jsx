9f50c0692cad4195f2a9a1509b1520853c2cd7c6
SiamIslamSagor <msiamislam12345@gmail.com>
2024-02-20 20:37:46 +0600

    added a commit to title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 const Title = ({ children }) => {
   return (
     <div>
+      {/* this is a title with bold style */}
       <h2 className="text-3xl py-10 font-bold">{children}</h2>
     </div>
   );
