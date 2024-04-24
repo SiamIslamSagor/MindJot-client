cbe710e1353a670934daa65b2eacca2cba1e12eb
SiamIslamSagor <msiamislam12345@gmail.com>
2024-04-24 21:50:53 +0600

    added text shadow on entry view text

--- src/components/ui/EntryView.jsx
+++ src/components/ui/EntryView.jsx
@@ -22,23 +22,29 @@ const EntryView = () => {
       >
         <div className="text-2xl -translate-y-10 2xl:-translate-y-20 borderrr sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-9xl uppercase text-center font-bold text-white px-2 flex flex-col gap-2">
           <div className="borderrr sm:mb-2 md:mb-3 lg:mb-5">
-            <h2 className="duration-500 text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-black inline font-extrabold  leading-6  md:leading-10 font-sans relative after:content-[''] after:absolute after:size-3 after:duration-500 after:rounded-[50%] after:bg-white after:-right-[13px] after:bottom-[6px] md:after:bottom-[8px] lg:after:bottom-[10px] after-animation-delay-class-2200 after:animate-[r2l-slide_1s_ease-in-out] ">
+            <h2 className="duration-500 text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-black inline font-extrabold  leading-6  md:leading-10 font-sans relative after:content-[''] after:absolute after:size-3 after:duration-500 after:rounded-[50%] after:bg-white after:-right-[13px] after:bottom-[6px] md:after:bottom-[8px] lg:after:bottom-[10px] bg---[#a39b9b] after-animation-delay-class-2200 after:animate-[r2l-slide_1s_ease-in-out] [text-shadow:1px_1px_0_#b91c1c,2px_2px_0_#b91c1c,3px_3px_0_#b91c1c,4px_4px_0_#b91c1c,5px_5px_0_#b91c1c] ">
               dev
             </h2>
           </div>
           <div className="flex items-center justify-center gap-5 borderr">
-            <h1 className="tracking-tight 2xl:leading-[150px]">Ideas</h1>
+            <h1 className="tracking-tight 2xl:leading-[150px] [text-shadow:1px_1px_0_#b91c1c,2px_2px_0_#b91c1c,3px_3px_0_#b91c1c,4px_4px_0_#b91c1c,5px_5px_0_#b91c1c,6px_6px_0_#b91c1c,7px_7px_0_#b91c1c,8px_8px_0_#b91c1c,9px_9px_0_#b91c1c]">
+              Ideas
+            </h1>
             <AttentionSeeker
               effect="rubberBand"
               delay={2500}
               className="text-black borderr max-lg:-mx-3  font-black"
             >
-              <h1 className="tracking-tight  2xl:leading-[150px]">&</h1>
+              <h1 className="tracking-tight  2xl:leading-[150px] [text-shadow:1px_1px_0_#b91c1c,2px_2px_0_#b91c1c,3px_3px_0_#b91c1c,4px_4px_0_#b91c1c,5px_5px_0_#b91c1c,6px_6px_0_#b91c1c,7px_7px_0_#b91c1c,8px_8px_0_#b91c1c,9px_9px_0_#b91c1c]">
+                &
+              </h1>
             </AttentionSeeker>
-            <h1 className="tracking-tight 2xl:leading-[150px]">Innovation</h1>
+            <h1 className="tracking-tight 2xl:leading-[150px] [text-shadow:1px_1px_0_#b91c1c,2px_2px_0_#b91c1c,3px_3px_0_#b91c1c,4px_4px_0_#b91c1c,5px_5px_0_#b91c1c,6px_6px_0_#b91c1c,7px_7px_0_#b91c1c,8px_8px_0_#b91c1c,9px_9px_0_#b91c1c]">
+              Innovation
+            </h1>
           </div>
           <div className="borderr ">
-            <h1 className="tracking-tight 2xl:leading-[150px]">
+            <h1 className="[textshadow:1px_1px_0_#d4d4d4,2px_2px_0_#d4d4d4,3px_3px_0_#d4d4d4,4px_4px_0_#d4d4d4,5px_5px_0_#d4d4d4,6px_6px_0_#d4d4d4,7px_7px_0_#d4d4d4,8px_8px_0_#d4d4d4,9px_9px_0_#d4d4d4] [text-shadow:1px_1px_0_#b91c1c,2px_2px_0_#b91c1c,3px_3px_0_#b91c1c,4px_4px_0_#b91c1c,5px_5px_0_#b91c1c,6px_6px_0_#b91c1c,7px_7px_0_#b91c1c,8px_8px_0_#b91c1c,9px_9px_0_#b91c1c] tracking-tight 2xl:leading-[150px]">
               MERN Stack <br className="max-lg:hidden 2xl:leading-[150px]" />{" "}
               <span className="mt-20 brdjcjer leading-6  md:leading-10 font-sans relative after:content-[''] after:absolute after:size-3 2xl:after:size-8 after:duration-500 after:rounded-[50%] after:bg-black after:-right-[15px] sm:after:-right-[18px] lg:after:-right-[20px] 2xl:after:-right-[38px] after:bottom-[4px] sm:after:bottom-[6px] md:after:bottom-[8px] lg:after:bottom-[12px] xl:after:bottom-[14px] 2xl:after:bottom-[30px] max-md:after:scale75 after-animation-delay-class-3000 after:animate-[bigDotBounce_1.5s_ease-in-out]">
                 Solutions
